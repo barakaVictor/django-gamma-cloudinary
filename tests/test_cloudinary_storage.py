be6f04b4a4ee9ce18dd495e95cde1c8d79a3d235
Victor Baraka <54454611+barakaVictor@users.noreply.github.com>
2021-10-21 14:06:48 +0300

    Update test_cloudinary_storage.py

--- tests/test_cloudinary_storage.py
+++ tests/test_cloudinary_storage.py
@@ -27,11 +27,40 @@ class CloudinaryStorageTestCase(SimpleTestCase):
     @patch('gamma_cloudinary.storage.cloudinary.uploader.upload')
     def test___save(self, mock_uploader):
         mock_uploader.return_value = {
-            'public_id':'test_resource_id',
-            'api_key': '88489930',
-            'secure_url': 'https://secure_url'
-            }
-        self.assertEqual(self.storage._save('css/test.css', ContentFile(b"these are bytes") ), 'test_resource_id')
+          "asset_id": "b5e6d2b39ba3e0869d67141ba7dba6cf",
+          "public_id": "eneivicys42bq5f2jpn2",
+          "version": 1570979139,
+          "version_id": "98f52566f43d8e516a486958a45c1eb9",
+          "signature": "abcdefghijklmnopqrstuvwxyz12345",
+          "width": 1000,
+          "height": 672,
+          "format": "jpg",
+          "resource_type": "image",
+          "created_at": "2017-08-11T12:24:32Z",
+          "tags": [],
+          "pages": 1,
+          "bytes": 350749,
+          "type": "upload",
+          "etag": "5297bd123ad4ddad723483c176e35f6e",
+          "placeholder": false,
+          "url": "http://res.cloudinary.com/demo/image/upload/v1570979139/eneivicys42bq5f2jpn2.jpg",
+          "secure_url": "https://res.cloudinary.com/demo/image/upload/v1570979139/eneivicys42bq5f2jpn2.jpg",
+          "access_mode": "public",
+          "original_filename": "sample",
+          "eager": [
+            { "transformation": "c_pad,h_300,w_400",
+              "width": 400,
+              "height": 300,
+              "url": "http://res.cloudinary.com/demo/image/upload/c_pad,h_300,w_400/v1570979139/eneivicys42bq5f2jpn2.jpg",
+              "secure_url": "https://res.cloudinary.com/demo/image/upload/c_pad,h_300,w_400/v1570979139/eneivicys42bq5f2jpn2.jpg" },
+            { "transformation": "c_crop,g_north,h_200,w_260",
+              "width": 260,
+              "height": 200,
+              "url": "http://res.cloudinary.com/demo/image/upload/c_crop,g_north,h_200,w_260/v1570979139/eneivicys42bq5f2jpn2.jpg",
+              "secure_url": "https://res.cloudinary.com/demo/image/upload/c_crop,g_north,h_200,w_260/v1570979139/eneivicys42bq5f2jpn2.jpg" }]
+        }
+        
+        self.assertEqual(self.storage._save('css/test.css', ContentFile(b"these are bytes") ), 'eneivicys42bq5f2jpn2')
 
     @patch('gamma_cloudinary.storage.requests.get')
     @patch('gamma_cloudinary.storage.CloudinaryStorage.url')
